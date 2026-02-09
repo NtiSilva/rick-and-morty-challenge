93dfaae80cac98a38e3e2242a584b5ff1d16c74f
NtiSilva <ntiSilva@users.noreply.github.com>
2026-02-09 10:02:00 -0300

    feat(hooks): create useCharacters hook with caching logic

---
+++ src/app/hooks/useCharacters.ts
@@ -0,0 +1,12 @@
+// src/hooks/useCharacters.ts
+import { useQuery } from '@tanstack/react-query';
+import { getCharacters } from '../services/api';
+
+export function useCharacters(page: number, searchTerm: string) {
+  return useQuery({
+    queryKey: ['characters', page, searchTerm], 
+    queryFn: () => getCharacters(page, searchTerm),
+    staleTime: 1000 * 30,
+    placeholderData: (previousData) => previousData,
+  });
+}
